be85597923e75654a24b51a269997fa7ffb6ac25
moritzHauptmann <mohauptmann@gmail.com>
2023-03-03 20:39:56 +1100

    changing httpLink and ws to use env variables

--- frontend/src/graphql/apollo-client.ts
+++ frontend/src/graphql/apollo-client.ts
@@ -8,7 +8,7 @@ const wsLink =
   typeof window !== 'undefined'
     ? new GraphQLWsLink(
         createClient({
-          url: 'ws://localhost:4000/graphql/subscriptions',
+          url: `ws://${process.env.BACKEND_URL}/graphql/subscriptions`,
           connectionParams: async () => ({
             session: await getSession(),
           }),
@@ -17,7 +17,7 @@ const wsLink =
     : null
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: `https://${process.env.BACKEND_URL}/graphql`,
   credentials: 'include',
 })
 
