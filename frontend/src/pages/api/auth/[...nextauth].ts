6ee11bcf160327a7e11741df9bf4f70b1077cf17
moritzHauptmann <mohauptmann@gmail.com>
2023-03-08 11:59:48 +1100

    set cookies less secure

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import GoogleProvider from 'next-auth/providers/google'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from '../../../lib/prismadb'
 
-export default NextAuth({
+export const nextAuthOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
@@ -17,4 +17,32 @@ export default NextAuth({
       return { ...session, user: { ...session.user, ...user } }
     },
   },
-})
+  debug: process.env.NODE_ENV === 'production',
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: false,
+        sameSite: 'none',
+        secure: false,
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        sameSite: 'none',
+        secure: false,
+      },
+    },
+    csrfToken: {
+      name: `__Host-next-auth.csrf-token`,
+      options: {
+        httpOnly: false,
+        sameSite: 'none',
+        secure: false,
+      },
+    },
+  },
+}
+
+export default NextAuth(nextAuthOptions)
